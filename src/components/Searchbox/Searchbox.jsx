c5b034e403fc7d7a6c366ed8dc1b3f576749a4ac
rafalwawrzyk <rafal.wawrzyk@gmail.com>
2018-06-27 19:35:59 +0200

    test PWA

--- src/components/Searchbox/Searchbox.jsx
+++ src/components/Searchbox/Searchbox.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const Searchbox = (props) => {
   return (
     <div className='pa2'>
-      <input type="text" className='pa3 ba b--green bg-lightest-blue' placeholder="Enter Robot Name" onChange={props.onChangeText}/>t
-      <label htmlFor=""></label>
+      <input aria-label='Search robots' type="search" className='pa3 ba b--green bg-lightest-blue' placeholder="Enter Robot Name" onChange={props.onChangeText}/>t
     </div>
   )
 }
