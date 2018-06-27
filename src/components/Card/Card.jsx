5fe09c22838771113ba8bbe220a624459730a052
rafalwawrzyk <rafal.wawrzyk@gmail.com>
2018-06-27 22:41:57 +0200

    update icons to PWA better rating

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({id,name,email}) => {
 	return (
 		<div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
 			<div>
-				<img src={`https://robohash.org/${id}?200x200`} alt="robots" />
+				<img src={`https://robohash.org/${id}?size=200x200`} alt="robots" />
 				<div>
 					<h2>{name}</h2>
 					<p>{email}</p>
--- src/components/Searchbox/Searchbox.jsx
+++ src/components/Searchbox/Searchbox.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Searchbox = (props) => {
   return (
     <div className='pa2'>
-      <input aria-label='Search robots' type="search" className='pa3 ba b--green bg-lightest-blue' placeholder="Enter Robot Name" onChange={props.onChangeText}/>t
+      <input aria-label='Search robots' type="search" className='pa3 ba b--green bg-lightest-blue' placeholder="Enter Robot Name" onChange={props.onChangeText}/>
     </div>
   )
 }
